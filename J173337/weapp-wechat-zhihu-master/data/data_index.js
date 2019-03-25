d25a6744ecc6207730e98cfe0697c2d0f1681659
吴端淞 <48464490+wds937@users.noreply.github.com>
2019-03-25 11:55:01 +0800

    Add files via upload

---
+++ J173337/weapp-wechat-zhihu-master/data/data_index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Rebecca_Han on 16/10/26.
+ */
+module.exports = {
+
+}
+
+var index= {
+    "id": 1,
+        "data": [
+        {
+            "question_id": 1,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Rebecca",
+            "feed_source_txt": "赞了回答1",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "选择 Kindle 而不是纸质书的原因是什么？",
+            "answer_ctnt": "难道不明白纸质书更贵啊！！！ 若觉得kindle更贵，我觉得要么阅读量太少，那确实没有买kindle的必要。要么买的都是盗版的纸质书？我不清楚不加以评论。。。 另外，用kindle看小说的怎么真心不懂了...",
+            "good_num": "112",
+            "comment_num": "18"
+         }
+
+    ]
+
+}
+
+module.exports.index = index;
